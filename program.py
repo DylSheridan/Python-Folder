42a8ddfd73b0704d9e43b56efb2ac0b10ebf5936
DylSheridan <ponrajad@sheridancollege.ca>
2024-10-02 16:02:52 -0400

    Interface

--- program.py
+++ program.py
@@ -2,4 +2,10 @@
 # PROG10004
 # Dylan Ponrajah
 
-print("================== ROCK PAPER SCISSORS ==================")
+#User Interface and Rules 
+print("================== ROCK PAPER SCISSORS ==================\n")
+print("Welcome to Rock Paper Scissors!\n")
+print("Rules:\nScissors beat paper, paper beats rock, rock beats scissors.\n")
+
+#Asking the user how many rounds must be won in order to win the match
+rounds = int(input("How many rounds must a player win to win the match?"))
